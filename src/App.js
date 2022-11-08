728971952fad72337b91de67c9591054a3a5bd50
Rajendra <itsrajendrapatel@gmail.com>
2022-11-08 18:49:39 +0530

    Project Started

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome Team</h1>
+      <p>Make Your respective pages and Link it over here.</p>
+    </div>
+  );
+}
+
+export default App;
