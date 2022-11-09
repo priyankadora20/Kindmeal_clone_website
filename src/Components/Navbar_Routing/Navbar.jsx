837d6a40c6c950f6f657562790d2603c0ab3cd71
Rajendra <itsrajendrapatel@gmail.com>
2022-11-09 18:51:17 +0530

    Login and Signup Button Functionality Completed

--- src/Components/Navbar_Routing/Navbar.jsx
+++ src/Components/Navbar_Routing/Navbar.jsx
@@ -2,6 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { useRef } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  Box,
+  Image,
+  Text,
+  Grid,
+} from "@chakra-ui/react";
 
 const Navbar = () => {
   const NavRef = useRef();
@@ -9,6 +24,8 @@ const Navbar = () => {
   const showNavbar = () => {
     NavRef.current.classList.toggle("showMenu");
   };
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <div className="Maindiv">
@@ -23,16 +40,21 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="social">
-          <img
-            src="https://www.kindmeal.my/images/follow_blog_grey.png"
-            alt=""
-          />
+          {/* firstimg */}
+          <a href="#">
+            <img
+              src="https://www.kindmeal.my/images/follow_blog_grey.png"
+              alt=""
+            />
+          </a>
+          {/* secondimg */}
           <a href="https://www.facebook.com/KindMeal.my" target="_blank">
             <img
               src="https://www.kindmeal.my/images/follow_facebook_grey.png"
               alt=""
             />
           </a>
+          {/* thirdimg */}
           <a href="https://twitter.com/KindMeal" target="_blank">
             <img
               src="https://www.kindmeal.my/images/follow_twitter_grey.png"
@@ -41,19 +63,124 @@ const Navbar = () => {
           </a>
         </div>
         <div className="login_signup">
-          <Link to={"/login"}>
-            <button>Login</button>
-          </Link>
-          <Link to={"/Signup"}>
-            <button>Signup</button>
-          </Link>
+          <button>Login</button>
+          <button onClick={onOpen}>Signup</button>
         </div>
+        {/* POPUP DIALOG OF THE WEBSITE  */}
+
+        <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            {/* <ModalCloseButton /> */}
+            <ModalBody pb={6}>
+              {/* MAIN HEADING OF THE WEBSITE  */}
+              <Box
+                boxShadow="sm"
+                p="6"
+                rounded="md"
+                bg="white"
+                textAlign={"center"}
+              >
+                <Image
+                  src="https://www.kindmeal.my/images/logo-kindmeal.png"
+                  alt="Dan Abramov"
+                />
+                <Text fontSize="xl" fontWeight={"bold"}>
+                  Join KindMeal For FREE Now
+                </Text>
+                <Text fontSize="sm">
+                  Please select the type of membership to proceed
+                </Text>
+              </Box>
+              {/* first Box of the Account */}
+              <Box
+                boxShadow="dark-lg"
+                p="2"
+                rounded="md"
+                bg="white"
+                mb={5}
+                onClick={() => {
+                  console.log("Hello Rahendra Patel");
+                }}
+                cursor="pointer"
+                alignContent={"center"}
+              >
+                <Grid
+                  templateColumns={{
+                    sm: "repeat(1, 1fr)",
+                    md: "repeat(2, 1fr)",
+                    lg: "repeat(2, 1fr)",
+                  }}
+                  gap={5}
+                >
+                  <Image
+                    src="https://www.kindmeal.my/images/join_normal.png"
+                    alt="Dan Abramov"
+                    margin={"auto"}
+                  />
+                  <Box textAlign={"center"}>
+                    <Text fontSize="2xl" color="red.500" mb={2}>
+                      Food Lover
+                    </Text>
+                    <Text fontSize="md" color="orange.500" lineHeight={"6"}>
+                      Join FREE to enjoy yummy deals, share reviews & meet our
+                      community of compassionate food lovers.
+                    </Text>
+                  </Box>
+                </Grid>
+              </Box>
+              {/* SECOND BOX OF THE ACCOUNT  */}
+              <Box
+                boxShadow="dark-lg"
+                p="2"
+                rounded="md"
+                bg="white"
+                mb={5}
+                onClick={() => {
+                  console.log("Hello Rahendra Patel");
+                }}
+                cursor="pointer"
+              >
+                <Grid
+                  templateColumns={{
+                    sm: "repeat(1, 1fr)",
+                    md: "repeat(2, 1fr)",
+                    lg: "repeat(2, 1fr)",
+                  }}
+                  gap={5}
+                >
+                  <Image
+                    src="https://www.kindmeal.my/images/join_shop.png"
+                    alt="Dan Abramov"
+                    margin={"auto"}
+                  />
+                  <Box textAlign={"center"}>
+                    <Text fontSize="2xl" color="red.500" mb={2}>
+                      Restaurant Owner
+                    </Text>
+                    <Text fontSize="md" color="orange.500" lineHeight={"6"}>
+                      List your food outlet, create exciting vegetarian deals &
+                      showcase your delicious menu for FREE.
+                    </Text>
+                  </Box>
+                </Grid>
+              </Box>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+
+        {/* MENU ICONS OF THE WEBSITE  */}
         <div className="hamburger">
           <span className="material-symbols-outlined" onClick={showNavbar}>
             menu
           </span>
         </div>
       </div>
+      {/* SECOND BOX OF THE WEBSITE  */}
       <div className="secondDiv showMenu" ref={NavRef}>
         <ul>
           <li>
@@ -77,12 +204,8 @@ const Navbar = () => {
             </Link>
           </li>
           <div className="login_signup_insider">
-            <Link to={"/login"}>
-              <button>Login</button>
-            </Link>
-            <Link to={"/Signup"}>
-              <button>Signup</button>
-            </Link>
+            <button>Login</button>
+            <button onClick={onOpen}>Signup</button>
           </div>
         </ul>
       </div>
