002ce9f67e49f349bdcb0cf57ce5d4ff0299f6b3
priyankadora20 <prdora97@gmail.com>
2022-11-09 21:34:34 +0530

    newupdate

---
+++ src/Components/Homepage/homepagemapdata.jsx
@@ -0,0 +1,33 @@
+const datahompage=[
+    {
+        id:1,
+        category:"Latest News & Videos",
+        img:"../Homepageimage/elephant.png",
+        describe:"Mama And Baby Elephant Get Stuck In 7 Foot Hole",
+    },
+    {
+        id:2,
+        category:"Latest News & Videos",
+        img:"../Homepageimage/pegion.png",
+        describe:"Watch These White Fluffy Babies Turn Into The Most Gorgeous ",
+    },
+    {
+        id:3,
+        category:"Latest News & Videos",
+        img:"../Homepageimage/beans.png",
+        describe:"Goulash & Dumplings From Speedy Bosh!",
+    },
+    {
+        id:4,
+        category:"Latest News & Videos",
+        img:"../Homepageimage/cow.png",
+        describe:"Best Friends At Off The Plate Sanctuary",
+    },
+    {
+        id:5,
+        category:"Latest News & Videos",
+        img:"../Homepageimage/seal.png",
+        describe:"A Sealabration Of Ocean Pups",
+    }
+]
+export default datahompage;
