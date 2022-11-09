82d5700a405559662cee24880e44302a0cea88a0
priyankadora20 <prdora97@gmail.com>
2022-11-10 00:49:42 +0530

    update

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,24 +1,73 @@
 import React from 'react';
 import './homepage.css';
 import datahompage from './homepagemapdata';
+import MainpageCarosole from './MainpageCarosole';
 
 
 export const Homepage = () => {
+   
   return (
-    <>
+    <> 
+        <MainpageCarosole/>
+        
         <div className='homepage_box1'>
             {datahompage.map(({id,category,img,describe})=>{
                 if(category==="Latest News & Videos"){
-                    <div key={id}>
-
-                        <img src={img}/>
-                        <p>{describe}</p>
-                    </div>
+                   return(
+            <div key={id}>
+            <img src={img}/>
+            <p>{describe}</p>
+            </div>
+                   ) 
                 }
             })}
 
         </div>
+        
+        <div>
+        {datahompage.map(({id,category,img,describe})=>{
+                if(category==="Discover Restaurants"){
+                   return(
+            <div key={id}>
+            <img src={img}/>
+            <p>{describe}</p>
+            </div>
+                   ) 
+                }
+            })}
+        </div>
 
+
+        <div>
+        {datahompage.map(({id,category,img,describe})=>{
+                if(category==="Yummylicious Moments"){
+                   return(
+            <div key={id}>
+            <img src={img}/>
+            <p>{describe}</p>
+            </div>
+                   ) 
+                }
+            })}
+        </div>
+
+
+        <div>
+        {datahompage.map(({id,category,img,describe})=>{
+                if(category==="Amazing Superheroes"){
+                   return(
+            <div key={id}>
+            <img src={img}/>
+            <p>{describe}</p>
+            </div>
+                   ) 
+                }
+            })}
+        </div>
+
+        <div>
+            /
+        </div>
     </>
   )
 }
