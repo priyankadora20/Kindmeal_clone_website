02a26376260927622139893f09f67fa3538f1ac0
priyankadora20 <prdora97@gmail.com>
2022-11-11 09:09:19 +0530

    new

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -6,19 +6,7 @@ import datahompage from './homepagemapdata';
 export const Homepage = () => {
   return (
     <>
-        <div className='homepage_box1'>
-            {datahompage.map(({id,category,img,describe})=>{
-                if(category==="Latest News & Videos"){
-                    <div key={id}>
-
-                        <img src={img}/>
-                        <p>{describe}</p>
-                        <div></div>
-                    </div>
-                }
-            })}
-
-        </div>
+       
 
     </>
   )
