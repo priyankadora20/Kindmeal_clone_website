16759648c715f85417d70a497581729028e0d929
priyankadora20 <prdora97@gmail.com>
2022-11-11 10:08:15 +0530

    new

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import './homepage.css';
 import datahompage from './homepagemapdata';
-
+import MainpageCarosole from './MainpageCarosole';
+import featurep from '../Homepageimage/featurep.png';
+import icon1 from '../Homepageimage/icon1.png';
+import icon2 from '../Homepageimage/icon2.png';
+import icon3 from '../Homepageimage/icon3.png';
+import icon4 from '../Homepageimage/icon4.png';
 
