b493425a67faf62a23cb59e72e1ae0ac6377d531
Swarooptheja <swarooptheja809@gmail.com>
2022-12-21 23:42:10 +0530

    added

--- src/Components/Directory/FeaturedRestaurant.jsx
+++ src/Components/Directory/FeaturedRestaurant.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 import "./FeaturedRestaurant.css"
 export const FeaturedRestaurant = () => {
+    let state=useSelector((state)=>{
+        console.log(state)
+    })
   return (
     <>
         <div id="featuredpagemain">
-            featured page to updated to my branch
+            <h1>
+                Featured restaurant
+            </h1>
         </div>
     </>
   )
